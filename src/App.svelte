9610aef45fbcca1a69f649e901d7b02666c088ab
joointhezoo <488654@gmail.com>
2020-09-09 03:43:03 +0900

    :new: add header Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,23 @@
 <script>
 	export let name;
+	import Header from './components/Header.svelte';
 </script>
-
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <Header />
+    <content>
+        <h1>Hello {name}!</h1>
+        <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    </content>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
 	}
-
+    content {
+        width: 100%;
+        text-align: center;
+    }
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+    let src = "/favicon.png";
+</script>
+
+<header>
+    <img {src} alt="logo" /> VELTE-TIME
+</header>
+
+<style>
+	header {
+	    display: flex;
+	    align-items: center;
+	    height: 60px;
+	    background: #676778;
+	    padding: 0 16px;
+	    color: #fff;
+	    font-weight: 500;
+	    letter-spacing: 2px;
+	}
+	img {
+	    width: 18px;
+	    margin: 0 2px 0 0;
+	}
+</style>
